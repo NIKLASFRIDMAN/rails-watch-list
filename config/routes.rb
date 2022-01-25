7e9c945dce6093419e8196086fff3146ffbac668
NIKLAS FRIDMAN <fridman.niklas@gmail.com>
2022-01-25 19:53:43 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
   resources :lists, only: [ :new, :create, :edit, :show, :index, :destroy ] do
     resources :bookmarks, only: [:index, :new, :create, :destroy]
   end
